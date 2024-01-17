8d20b07e46ce3eba9b61b4b0b40d4c9eedbe0bcb
Roland Reagan <webdotwork@gmail.com>
2024-01-17 22:15:59 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+function greet(name) {
+  return `Hello, ${name}`
+}
+// prettier-ignore
+console.log(greet("my World!"));
+// $ node index.js
+// Hello, world!
+// cp index.js main.js
+// $ node main.js
+// Hello, world!
